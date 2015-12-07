cadaf21a6dc92fae233a804f15330cbdd4678a61
ken <one.2426@gmail.com>
2015-12-07 09:20:53 +0800

    上げ直し

--- bbs.php
+++ bbs.php
@@ -1,8 +1,14 @@
+
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset='UTF-8'>
   <title>oneline</title>
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" href="assets/css/form.css">
+  <link rel="stylesheet" href="assets/css/main.css">
+  <link rel="stylesheet" href="assets/css/timeline2.css">
 </head>
 <body>
   <form method='post' action='bbs.php'>
@@ -11,65 +17,61 @@
     <button type="submit">つぶやく</button>
   </form>
 
+    <?php
+      // POST送信が行われたら、下記の処理を実行
+      // テストコメント
+      if(isset($_POST) && !empty($_POST)){
 
-  <h2><a href="#">nickname KEN</a> <span>2015-12-02 10:10:20</span></h2>
-  <p>つぶやきコメント</p>
-
-  <h2><a href="#">nickname KEN</a> <span>2015-12-02 10:10:10</span></h2>
-  <p>つぶやきコメント２</p>
-
-
-
-<?php
-  // POST送信が行われたら、下記の処理を実行
-  // テストコメント
-  if(isset($_POST) && !empty($_POST)){
-
-    // データベースに接続
-    $dsn='mysql:dbname=oneline_bbs;host=localhost';
-    $User='root';
-    $password='';
-    $dbh= new PDO($dsn,$User,$password);
-    $dbh->query('SET NAMES utf8');
-
-    $nickname = $_POST['nickname'];
-    $comment = $_POST['comment'];
-
-    // SQL分作成(INSERT文)
-    $sql = 'INSERT INTO `posts` (`id`, `nickname`, `comment`, `created`) VALUES (NULL, "'.$nickname.'","'.$comment.'", now())';
-    $stmt=$dbh->prepare($sql);
-    // insert文実行
-    $stmt->execute();
-
-    $sql = 'SELECT * FROM posts WHERE 1';
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute();
+        // データベースに接続
+        // $dsn='mysql:dbname=oneline_bbs;host=localhost';
+        // $User='root';
+        // $password='';
 
 
-    echo '<table border=1 >';
-    while(1){
-      $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-      if($rec==false){
-        break;
+        $dsn='mysql:dbname=LAA0686086;host= mysql105.phy.lolipop.lan';
+        $User='LAA0686086';
+        $password='nexseed1204';
+        $dbh= new PDO($dsn,$User,$password);
+        $dbh->query('SET NAMES utf8');
+
+        $nickname = $_POST['nickname'];
+        $comment = $_POST['comment'];
+
+        // SQL分作成(INSERT文)
+        $sql = 'INSERT INTO `posts` (`id`, `nickname`, `comment`, `created`) VALUES (NULL, "'.$nickname.'","'.$comment.'", now())';
+        $stmt=$dbh->prepare($sql);
+        // insert文実行
+        $stmt->execute();
+
+        $sql = 'SELECT * FROM posts WHERE 1';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+
+
+        echo '<table border=1 >';
+        while(1){
+          $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+          if($rec==false){
+            break;
+          }
+            echo '<tr>';
+            echo '<td>'.$rec['id'].'</td>';
+            echo '<td>'.$rec['nickname'].'</td>';
+            echo '<td>'.$rec['comment'].'</td>';
+            echo '<td>'.$rec['created'].'</td>';
+            echo'</tr>';
+        }
+        echo '</table>';
       }
-      echo '<tr>';
-        echo '<td>'.$rec['id'].'</td>';
-        echo '<td>'.$rec['nickname'].'</td>';
-        echo '<td>'.$rec['comment'].'</td>';
-        echo '<td>'.$rec['created'].'</td>';
-        echo'</tr>';
-    }
-    echo '</table>';
-  }
 
 
 
 
 
-  // データベースから切断
-  $dbn = null;
+      // データベースから切断
+      $dbn = null;
 
-?>
+    ?>
 
-</body>
+<!-- </body>
 </html>
